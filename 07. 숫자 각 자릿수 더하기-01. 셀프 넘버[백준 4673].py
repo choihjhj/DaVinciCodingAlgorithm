f7a7a3082c175794af70fb8c046b9c29b3eb83d5
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-12 17:40:59 +0900

    Update 07. 숫자 각 자릿수 더하기-01. 셀프 넘버[백준 4673].py

--- 07. 숫자 각 자릿수 더하기-01. 셀프 넘버[백준 4673].py
+++ 07. 숫자 각 자릿수 더하기-01. 셀프 넘버[백준 4673].py
@@ -1,9 +1,12 @@
+'''
+셀프넘버가 아닌 수는 자기자신+각자릿수의합
+'''
 A = set(range(1, 10000))
 B = set()  #중복생성자 없도록 하기위해서
 for x in A :
     for y in str(x):
         x += int(y)
-    B.add(x)  
-C = A - B  
+    B.add(x)  #셀프넘버 아닌 수
+C = A - B  #셀프넘버
 for x in sorted(C):  
     print(x)

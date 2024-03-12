78f17b6d534d6362f54005003a736759330eda0f
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-12 17:32:22 +0900

    Update 07. 숫자 각 자릿수 더하기-02. 신기한 수(정올 2019)[백준 17618].py

--- 07. 숫자 각 자릿수 더하기-02. 신기한 수(정올 2019)[백준 17618].py
+++ 07. 숫자 각 자릿수 더하기-02. 신기한 수(정올 2019)[백준 17618].py
@@ -6,3 +6,22 @@ for i in range(1, n+1):
     tmp=sum([int(c) for c in str(i)])
     if i % tmp == 0: cnt+=1
 print(cnt)
+
+#78점, Python3 	31120kb	496ms
+N = int(input())
+
+def solve(n):
+    si = str(n)
+    tmp = 0
+    for s in si:
+        tmp += int(s)
+    if n % tmp == 0:
+        return True
+    return False
+
+ans = 0
+for i in range(1, N+1):
+    if solve(i):
+        ans += 1
+
+print(ans)

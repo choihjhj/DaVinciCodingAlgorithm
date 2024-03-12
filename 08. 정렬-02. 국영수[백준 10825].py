6b3e57e5ad46c7b2707e222a68af8ead107bb2bc
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-12 18:47:01 +0900

    Update 08. 정렬-02. 국영수[백준 10825].py

--- 08. 정렬-02. 국영수[백준 10825].py
+++ 08. 정렬-02. 국영수[백준 10825].py
@@ -6,7 +6,7 @@ for _ in range(N):
     name, *scores = sys.stdin.readline().rstrip().split()
     arr.append((name, *map(int, scores)))
 
-arr.sort(key=lambda x : (-x[1], x[2], -x[3], x[0]))
+arr.sort(key=lambda x : (-x[1], x[2], -x[3], x[0])) #-내림차순, +오름차순
 
 for st in arr:
     print(st[0])

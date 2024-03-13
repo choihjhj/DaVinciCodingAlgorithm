f100343c85bca566efe35d1ae061b7c166c32737
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-13 18:48:29 +0900

    Update 11. 브루트 포스.py

--- 11. 브루트 포스.py
+++ 11. 브루트 포스.py
@@ -4,3 +4,12 @@
 조합(Combination)-순서없음
 from itertools import combinations
 list(combinations(리스트, 조합수))
+조합 개수 nCr= n*...*(n-r-1)/(r*..*1)
+10C3=10 * 9 * 8 / (3 * 2 * 1)
+
+순열(Permutation)-순서있음, 모든 경우의 수
+from itertools import permutations
+list(permutations(리스트, 순열수))
+순열 개수 nPr = n*...*(n-r-1)
+10P3=10*9*8
+

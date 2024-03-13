c247e70ec047c57802235f38fd5c33098d9b769d
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-13 18:37:50 +0900

    Create 11. 브루트 포스.py

---
+++ 11. 브루트 포스.py
@@ -0,0 +1,6 @@
+브루트 포스(Brute Force)는 알고리즘이라기 보다는 모든 경우를 다 따지며 해를 찾는 방식
+문제를 어떻게 풀어야 할지 모를 때 브루트 포스로 문제를 풀다보면 해결할 방법이 보일 수도 있고, 부분점수라도 얻을 수 있기 때문에 꼭 알아두어야 함
+
+조합(Combination)-순서없음
+from itertools import combinations
+list(combinations(리스트, 조합수))

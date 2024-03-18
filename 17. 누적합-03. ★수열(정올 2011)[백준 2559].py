2ea11d644fab2e81583fde66343e5096de974f5c
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-19 00:21:45 +0900

    Update 17. 누적합-03. ★수열(정올 2011)[백준 2559].py

--- 17. 누적합-03. ★수열(정올 2011)[백준 2559].py
+++ 17. 누적합-03. ★수열(정올 2011)[백준 2559].py
@@ -1,18 +1,14 @@
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
-
 sa = []
 total = 0
 for a in arr:
     total += a
     sa.append(total)
-
-result = sa[K - 1]
+maxx = sa[K - 1]
 for i in range(K, N):
-    temp = sa[i] - sa[i - K]
-    result = max(result, temp)
-print(result)
-
+    maxx = max(maxx, sa[i] - sa[i - K])
+print(maxx)
 
 # 슬라이딩 윈도우로 풀었을 때 코드
 N, K = map(int, input().split())

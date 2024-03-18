a245526c7c27732ec41b652da8e34a827a15a146
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-19 01:31:53 +0900

    Update 18. 슬라이딩윈도우-01. 블로그[백준 21921].py

--- 18. 슬라이딩윈도우-01. 블로그[백준 21921].py
+++ 18. 슬라이딩윈도우-01. 블로그[백준 21921].py
@@ -2,18 +2,17 @@ N, X = map(int, input().split())
 arr = list(map(int, input().split()))
 sw = sum(arr[:X])
 max_sum = sw
-max_cnt = 1
+cnt = 1
 for i in range(X, N):
-    sw -= arr[i - X]
-    sw += arr[i]
-    if sw == max_sum:
-        max_cnt += 1
-    elif max_sum < sw:
+    sw = sw - arr[i - X] + arr[i]
+    if sw == max_sum: #최대합 같은거 발견하면 cnt 1증가
+        cnt += 1
+    elif max_sum < sw: #더 큰거 발견하면 max값 바꾸고 cnt=1로 셋팅
         max_sum = sw
-        max_cnt = 1
+        cnt = 1
 
 if max_sum == 0:
     print("SAD")
 else:
     print(max_sum)
-    print(max_cnt)
+    print(cnt)

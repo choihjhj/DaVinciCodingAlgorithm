4555aeff5c4e9c978afb7f87f24bf2eb315c955b
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-18 23:48:04 +0900

    Update 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py

--- 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py
+++ 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py
@@ -1,16 +1,15 @@
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-arr = []
+arr = [[0]*(n+1)]
 for i in range(n):
-    a = list(map(int, input().split()))
-    arr.append(a)
+    arr.append([0]+list(map(int, input().split())))
     
 dp = [[0]*(n+1) for i in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] + arr[i-1][j-1] - dp[i-1][j-1]
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] + arr[i][j] - dp[i-1][j-1]
         
 for k in range(m):
     x1,y1,x2,y2 = map(int,input().split())

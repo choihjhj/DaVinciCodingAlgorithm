a98dbd5ed7893ac89c17868868a5d3c676aca470
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-18 23:52:16 +0900

    Update 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py

--- 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py
+++ 17. 누적합-02. ★★구간 합 구하기 5[백준 11660].py
@@ -2,6 +2,7 @@
 2차원 배열이라 까다로운 문제임
 합 구할 때 dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + arr[i][j] 이렇게 해야 하고
 출력힐 때도 dp[x2][y2] - dp[x2][y1-1] -dp[x1-1][y2] + dp[x1-1][y1-1]
+참고링크) https://youtu.be/irLF8gaAoGk?si=YB39OB5nWJq_gBBD
 '''
 import sys
 input = sys.stdin.readline

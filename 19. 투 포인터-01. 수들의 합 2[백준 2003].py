e912b9af427c422a70fdf4df80580b95280f62fa
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-20 19:46:09 +0900

    Update 19. 투 포인터-01. 수들의 합 2[백준 2003].py

--- 19. 투 포인터-01. 수들의 합 2[백준 2003].py
+++ 19. 투 포인터-01. 수들의 합 2[백준 2003].py
@@ -1,3 +1,4 @@
+# 풀이1) 다빈치 코딩 알고리즘 풀이
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
@@ -18,3 +19,16 @@ while True:
     if sum_arr == M:
         ans += 1
 print(ans)
+
+# 풀이2) 이코테 책풀이
+n,m=map(int, input().split())
+arr=list(map(int, input().split()))
+e,cnt,summ=0,0,0
+for s in range(n):  #s를 차례대로 증가시키며 반복
+  while e<n and summ<m: #e를 가능한 만큼 이동시키
+    summ+=arr[e]
+    e+=1
+  if summ==m: #부분합이 m일 때 카운트 증
+    cnt+=1
+  summ-=arr[s]
+print(cnt)

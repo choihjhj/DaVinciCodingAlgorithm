7e25bb151a61ff6b288cd7135659af81dba36985
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-20 22:13:48 +0900

    Update 19. 투 포인터-01. 수들의 합 5[백준 2018].py

--- 19. 투 포인터-01. 수들의 합 5[백준 2018].py
+++ 19. 투 포인터-01. 수들의 합 5[백준 2018].py
@@ -1,3 +1,17 @@
+# 풀이1) 이코테 방법
+import sys
+n=int(sys.stdin.readline()) #15
+sum,end,cnt=0,0,0
+for start in range(1,n+1):
+    while end<=n and sum<n:
+        sum+=end
+        end+=1
+    if sum==n:
+        cnt+=1
+    sum-=start
+print(cnt) #4   
+
+# 풀이2) 다빈치코딩알고리즘 풀이
 n=int(input())
 arr=range(1,n+1)
 summ=0

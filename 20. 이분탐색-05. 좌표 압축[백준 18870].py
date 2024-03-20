c51095c8c8669087784ad27cde66e14d2fd1d31b
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-21 04:37:09 +0900

    Update and rename 20. 이분탐색-좌표 압축[백준 18870].py to 20. 이분탐색-05. 좌표 압축[백준 18870].py

---
+++ 20. 이분탐색-05. 좌표 압축[백준 18870].py
@@ -0,0 +1,16 @@
+# 이분탐색 bisect 모듈 이용 , PyPy3 274784kb	1176ms	
+from bisect import bisect_left
+N=int(input())
+a=list(map(int, input().split()))
+b=sorted(list(set(a)))
+for i in a:
+    idx=bisect_left(b,i)
+    print(idx,end=' ')
+
+# dict 이용, 308100kb	1152ms
+from bisect import bisect_left
+N=int(input())
+a=list(map(int, input().split()))
+b={x:i for i, x in enumerate(sorted(list(set(a))))}
+for i in a:
+    print(b[i],end=' ')

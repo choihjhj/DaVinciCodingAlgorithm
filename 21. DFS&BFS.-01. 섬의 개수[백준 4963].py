f5c5eac1f2ad426a39f734d38728e887150d4181
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-22 00:39:38 +0900

    Update 21. DFS&BFS.-01. 섬의 개수[백준 4963].py

--- 21. DFS&BFS.-01. 섬의 개수[백준 4963].py
+++ 21. DFS&BFS.-01. 섬의 개수[백준 4963].py
@@ -1,3 +1,4 @@
+# DFS 풀이
 import sys
 sys.setrecursionlimit(10 ** 4) #재귀쓸 때 널기
 d=[(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)] #하부터 시계반대방향으로 순회, 하 대각선 우 대각선 상 대각선 좌 대각선
@@ -22,5 +23,39 @@ while True:
     for j in range(w):
       if arr[i][j] and not visited[i][j]:  # visited[i][j]==False 이고, arr[i][j]==1이면 탐색 후 cnt 섬증가
             dfs(i,j)
+            cnt+=1
+  print(cnt)
+
+# BFS 풀이
+import sys
+from collections import deque
+sys.setrecursionlimit(10 ** 4)
+d=[(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)] #하부터 시계반대방향으로 순회 ,하 대각선 우 대각선 상 대각선 좌 대각선
+def bfs(x, y):
+    visited[x][y]=True
+    que = deque()
+    que.append((x, y))
+    while que:
+        x, y = que.popleft()
+        for i in range(8):
+            nx = x + d[i][0]
+            ny = y + d[i][1]
+            if (0<= nx < h and 0<= ny < w):
+                if not visited[nx][ny] and arr[nx][ny]: # visited[nx][ny]==False 이고, arr[nx][ny]==1이면 다음위치 탐색
+                    visited[nx][ny]=True
+                    que.append((nx, ny))
+  
+while True:
+  w,h=map(int, sys.stdin.readline().rstrip().split())
+  if w==0 and h==0: 
+    break
+  visited =[ [False]*(w) for _ in range(h)]
+  cnt=0
+  
+  arr=[ tuple(map(int, sys.stdin.readline().rstrip().split())) for _ in range(h)]
+  for i in range(h):
+    for j in range(w):
+      if arr[i][j] and not visited[i][j]:  # visited[i][j]==False 이고, arr[i][j]==1이면 탐색 후 cnt 섬증가
+            bfs(i,j)
             cnt+=1
   print(cnt)

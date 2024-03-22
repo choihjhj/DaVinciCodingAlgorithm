c04a8d7350a5e6969779dfe29eb60641375ce676
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-22 19:26:44 +0900

    Update and rename 21. DFS&BFS-04. 토마토(정올 2013)[백준 7576].py to 21. DFS&BFS-04. ★토마토(정올 2013)[백준 7576].py

---
+++ 21. DFS&BFS-04. ★토마토(정올 2013)[백준 7576].py
@@ -0,0 +1,38 @@
+'''
+토마토가 모두 익을 때까지의 최소 날짜를 출력이니까 BFS로 풀기
+1 익은 토마토를 찾아 0안익은 토마토를 익혀나가야한다. -1은 빈칸임
+익은 토마토 위치 덱에 넣기
+'''
+from collections import deque
+M, N = map(int, input().split())
+arr = []
+queue = deque()
+for i in range(N):
+    row = list(map(int, input().split()))
+    arr.append(row)
+    for j in range(M):
+        if row[j] == 1: #익은 토마토 덱에 위치 넣기
+            queue.append((i, j))
+
+d = d=[(-1,0),(1,0),(0,-1),(0,1)] #상하좌우
+def bfs():  
+    while queue: #토마토 다 익힐때까지 반복 (0없애기)
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + d[i][0]
+            ny = y + d[i][1]
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0: #다음 위치가 안익은 토마토이면 익혀나가기
+                arr[nx][ny] = arr[x][y] + 1
+                queue.append((nx, ny)) #이것도 익은거니까 덱에 위치 넣기
+bfs()
+def get_day():
+    result = 0 #결과
+    for row in arr:
+        if 0 in row: #안익은토마토 하나라도 있으면 -1
+            return -1
+
+        row_max = max(row)    
+        if result < row_max:
+            result = row_max
+    return result - 1 #며칠 걸리냐 했으니까 -1 빼야 함 
+print(get_day())

a0966585fd42dfb002a2444c81f7b49f87c64e12
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-22 18:25:39 +0900

    Update 21. DFS&BFS-00.py

--- 21. DFS&BFS-00.py
+++ 21. DFS&BFS-00.py
@@ -1,3 +1,9 @@
+'''
+DFS는 [x][y]=0, for문, 재귀로 호출
+BFS는 deque, while문+방향순회for문, [x][y]=0,[nx][ny]=0 처리 큐에 popleft, append
+섬개수 세는 문제는 main에서 이중포문돌때 cnt+=1하면 됨(백준 4963 섬의개수 문제)
+만약 섬안의 집수까지 세는건 dfs함수에서 cnt=1만들고 재귀로 cnt+=dfs(nx,ny)해서 최종 return cnt 하면 됨(백준 2667 단지번호붙이기 문제)
+'''
 # DFS(깊이우선탐색)
 visited = [[False] * M for _ in range(N)]
 d=[(-1,0),(1,0),(0,-1),(0,1)] #상하좌우

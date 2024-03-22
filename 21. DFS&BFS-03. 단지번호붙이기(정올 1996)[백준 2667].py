3ec4413cca1743827242ca6c4142de7822f703ef
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-22 18:19:59 +0900

    Update 21. DFS&BFS-03. 단지번호붙이기(정올 1996)[백준 2667].py

--- 21. DFS&BFS-03. 단지번호붙이기(정올 1996)[백준 2667].py
+++ 21. DFS&BFS-03. 단지번호붙이기(정올 1996)[백준 2667].py
@@ -1,23 +1,19 @@
-N = int(input())
-arr = [list(input()) for _ in range(N)]
+N=int(input())
 d = [(-1,0), (1,0), (0,-1), (0,1)]
-def dfs(x, y):
-    arr[x][y] = "0"
-    cnt = 1
-    for k in range(4):
-        nx = x + d[k][0]
-        ny = y + d[k][1]
-        if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == "1":
-            cnt += dfs(nx, ny)
+arr=[list(input()) for _ in range(N)]
+def dfs(x,y):
+    arr[x][y]='0'
+    cnt=1
+    for i in range(4):
+        nx=x+d[i][0]
+        ny=y+d[i][1]
+        if (0<=nx<N and 0<=ny<N) and arr[nx][ny]=='1':
+            cnt+=dfs(nx,ny)           
     return cnt
 
-result = []
+result=[]
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == "1":
-            cnt = dfs(i, j)
-            result.append(cnt)
-
-print(len(result))
-for i in sorted(result):
-    print(i)
+        if arr[i][j]=='1':
+            result.append(dfs(i,j))            
+print(len(result), *sorted(result), sep='\n')

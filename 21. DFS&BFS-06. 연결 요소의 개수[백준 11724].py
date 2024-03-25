4e0e49013e735a376a88c8f3dc0350843b955432
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-25 18:28:25 +0900

    Update 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py

--- 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py
+++ 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py
@@ -1,3 +1,4 @@
+# 풀이1) DFS
 import sys
 sys.setrecursionlimit(10**4)
 N, M = map(int, sys.stdin.readline().rstrip().split()) #노드개수, 간선개수
@@ -16,5 +17,33 @@ visited = [False] * (N + 1)
 for i in range(1,N+1):
   if not visited[i]:
     dfs(i)
+    cnt+=1
+print(cnt)
+
+# 풀이2) BFS
+import sys
+from collections import deque
+N, M = map(int, sys.stdin.readline().rstrip().split()) #노드개수, 간선개수
+arr=[[] for _ in range(N+1)] #맨앞 빈배열[]
+for _ in range(M):
+  u, v = map(int, sys.stdin.readline().rstrip().split())
+  arr[u].append(v)
+  arr[v].append(u)
+  
+def bfs(node):
+  dq=deque([node])
+  visited[node] = True
+  while dq:
+    v=dq.popleft()
+    for next_node in arr[v]:
+        if not visited[next_node]:
+            dq.append(next_node)
+            visited[next_node]=True
+          
+cnt=0
+visited = [False] * (N + 1)
+for i in range(1,N+1):
+  if not visited[i]:
+    bfs(i)
     cnt+=1
 print(cnt)

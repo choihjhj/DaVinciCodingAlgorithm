ad8372010737663585f30c8f8aeeaec04e1448ec
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-26 19:39:59 +0900

    Update 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py

--- 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py
+++ 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py
@@ -1,21 +1,20 @@
 import sys
 sys.setrecursionlimit(10**6)
-input=sys.stdin.readline
-n=int(input().rstrip())
-chon1, chon2= map(int, input().rstrip().split())
+n=int(sys.stdin.readline().rstrip())
+chon1, chon2= map(int, sys.stdin.readline().rstrip().split())
 m=int(input().rstrip())
 arr=[[] for _ in range(n+1)] #맨앞 빈배열[]
 for _ in range(m):
-  u, v = map(int, input().rstrip().split())
+  u, v = map(int, sys.stdin.readline().rstrip().split())
   arr[u].append(v)
   arr[v].append(u)
-visited=[0]*(n+1)
+visited=[-1]*(n+1)
 
 visited[chon1]=0
 def dfs(node):
   if node==chon2: return
   for next in arr[node]:		
-    if not visited[next]:
+    if visited[next]==-1:
       visited[next]=visited[node]+1
       dfs(next)
 dfs(chon1)

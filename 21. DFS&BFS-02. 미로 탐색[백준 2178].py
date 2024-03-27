9bb3eb3f113dab61e2b8532c6db87e9e60a3d835
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-27 21:53:47 +0900

    Update 21. DFS&BFS-02. 미로 탐색[백준 2178].py

--- 21. DFS&BFS-02. 미로 탐색[백준 2178].py
+++ 21. DFS&BFS-02. 미로 탐색[백준 2178].py
@@ -3,6 +3,7 @@
 문자열로 확인할 것이고, visited로 칸수를 증가하며 (N,M)애 도달했을 때의 visited 결과값인 칸수를 체크할 것이다.
 시작점(0,0) 끝점(N,M) 결과가 궁금한 것이므로 이중포문 필요 x, 전부 bfs()로 순회해서 결과 저장해놓고 마지막칸만 확인하면 되니까
 '''
+# 풀이1) visited 사용, 	34052kb	76ms
 import sys
 from collections import deque
 N, M = map(int, sys.stdin.readline().rstrip().split())
@@ -24,3 +25,25 @@ def bfs():
 
 bfs()
 print(visited[N-1][M-1])
+
+# 풀이2) visited 사용 X ,34072kb	84ms
+import sys
+from collections import deque
+input=sys.stdin.readline
+d=[(-1,0),(1,0),(0,-1),(0,1)] #상하좌우
+N,M=map(int, input().rstrip().split())
+arr= [ list(map(int,input().rstrip())) for _ in range(N)]
+def BFS():
+    arr[0][0]=1
+    dq=deque()
+    dq.append((0,0))
+    while dq:
+        x,y=dq.popleft()
+        for i in range(4):
+            nx=x+d[i][0]
+            ny=y+d[i][1]
+            if(0<=nx<N and 0<=ny<M) and arr[nx][ny]==1:
+                arr[nx][ny]=arr[x][y]+1
+                dq.append((nx,ny))
+BFS()
+print(arr[N-1][M-1])

762dd47f2871502414ebf06048c3abd71b8ea2f4
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-28 03:19:50 +0900

    Update 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py

--- 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py
+++ 21. DFS&BFS-08. 촌수계산(정올 1999)[백준 2644].py
@@ -1,21 +1,39 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**6)
-n=int(sys.stdin.readline().rstrip())
-chon1, chon2= map(int, sys.stdin.readline().rstrip().split())
+input=sys.stdin.readline
+n=int(input().rstrip())
+chon1,chon2=map(int, input().rstrip().split())
 m=int(input().rstrip())
-arr=[[] for _ in range(n+1)] #맨앞 빈배열[]
-for _ in range(m):
-  u, v = map(int, sys.stdin.readline().rstrip().split())
-  arr[u].append(v)
-  arr[v].append(u)
 visited=[-1]*(n+1)
+arr=[[] for _ in range(n+1)]
+for _ in range(m):
+    u,v=map(int, input().rstrip().split())
+    arr[u].append(v)
+    arr[v].append(u)
 
-visited[chon1]=0
-def dfs(node):
-  if node==chon2: return
-  for next in arr[node]:		
-    if visited[next]==-1:
-      visited[next]=visited[node]+1
-      dfs(next)
-dfs(chon1)
-print(visited[chon2])
+def DFS(node):
+    if node==chon1: return
+    for next_node in arr[node]:
+        if visited[next_node]==-1:
+            visited[next_node]=visited[node]+1
+            DFS(next_node)
+
+def BFS(node):
+    dq=deque()
+    dq.append(node)
+    visited[node]=0
+    while dq:
+        v=dq.popleft()
+        if v==chon1: return
+        for next_node in arr[v]:       
+            if visited[next_node]==-1:
+                visited[next_node]=visited[v]+1
+                dq.append(next_node)
+    return -1
+
+visited[chon2]=0
+DFS(chon2)
+
+# BFS(chon2)
+print(visited[chon1])

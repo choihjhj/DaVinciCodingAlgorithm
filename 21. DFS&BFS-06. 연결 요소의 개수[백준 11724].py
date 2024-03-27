bbf6466871e80b0a2cf7e8fbfab5e80e0570be78
Hyeji Choi <hyejichoi14@gmail.com>
2024-03-28 00:39:05 +0900

    Update 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py

--- 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py
+++ 21. DFS&BFS-06. 연결 요소의 개수[백준 11724].py
@@ -1,49 +1,35 @@
-# 풀이1) DFS
-import sys
-sys.setrecursionlimit(10**4)
-N, M = map(int, sys.stdin.readline().rstrip().split()) #노드개수, 간선개수
-arr=[[] for _ in range(N+1)] #맨앞 빈배열[]
-for _ in range(M):
-  u, v = map(int, sys.stdin.readline().rstrip().split())
-  arr[u].append(v)
-  arr[v].append(u)
-def dfs(node):
-    visited[node] = True
-    for next_node in arr[node]:
-        if not visited[next_node]:
-            dfs(next_node)
-cnt=0
-visited = [False] * (N + 1)
-for i in range(1,N+1):
-  if not visited[i]:
-    dfs(i)
-    cnt+=1
-print(cnt)
-
-# 풀이2) BFS
 import sys
 from collections import deque
-N, M = map(int, sys.stdin.readline().rstrip().split()) #노드개수, 간선개수
-arr=[[] for _ in range(N+1)] #맨앞 빈배열[]
+sys.setrecursionlimit(10**6)
+input=sys.stdin.readline
+N,M=map(int, input().rstrip().split())
+visited=[0]*(N+1)
+arr=[[] for _ in range(N+1)]
 for _ in range(M):
-  u, v = map(int, sys.stdin.readline().rstrip().split())
-  arr[u].append(v)
-  arr[v].append(u)
+    u,v=map(int, input().rstrip().split())
+    arr[u].append(v)
+    arr[v].append(u)
   
-def bfs(node):
-  dq=deque([node])
-  visited[node] = True
-  while dq:
-    v=dq.popleft()
-    for next_node in arr[v]:
-        if not visited[next_node]:
-            dq.append(next_node)
-            visited[next_node]=True
-          
+def DFS(node):
+    visited[node]=1
+    for i in arr[node]:
+        if not visited[i]:
+            DFS(i)    
+
+def BFS(node):
+    visited[node]=1
+    dq=deque()
+    dq.append(node)
+    while dq:
+        for i in arr[dq.popleft()]:
+            if not visited[i]:
+                dq.append(i)
+                visited[i]=1    
+
 cnt=0
-visited = [False] * (N + 1)
 for i in range(1,N+1):
-  if not visited[i]:
-    bfs(i)
-    cnt+=1
+    if not visited[i]: 
+        DFS(i)
+        # BFS(i)
+        cnt+=1    
 print(cnt)

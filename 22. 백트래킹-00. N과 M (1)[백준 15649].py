c32dcb0c27201059c87d7764beb9603caae993be
Hyeji Choi <hyejichoi14@gmail.com>
2024-04-04 19:32:32 +0900

    Update 22. 백트래킹-00. N과 M (1)[백준 15649].py

--- 22. 백트래킹-00. N과 M (1)[백준 15649].py
+++ 22. 백트래킹-00. N과 M (1)[백준 15649].py
@@ -4,3 +4,18 @@ N, M = map(int, input().split())
 arr = list(range(1, N+1))
 for i in permutations(arr, M):
   print(*i,end='\n')
+
+# 풀이2) 백트래킹
+def dfs():
+    if len(arr) == M:
+        print(" ".join(map(str, arr)))
+        return
+
+    for i in range(1, N+1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+N, M = map(int,input().split())
+arr = []
+dfs()

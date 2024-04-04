ece322b2573510e4381edd0ea3a69ca51814f705
Hyeji Choi <hyejichoi14@gmail.com>
2024-04-05 03:21:35 +0900

    Update 22. 백트래킹-01. 근손실[백준 18429].py

--- 22. 백트래킹-01. 근손실[백준 18429].py
+++ 22. 백트래킹-01. 근손실[백준 18429].py
@@ -1,12 +1,12 @@
 '''브루트포스 알고리즘 + 백트래킹 문제'''
-풀이1) 순열 permutations 이용
+# 풀이1) 순열 permutations 이용
 from itertools import permutations
 import sys
 input=sys.stdin.readline
 N,K=map(int, input().split())
-arr=list(map(int, input().split()))
+A = list(map(int, input().split()))
 cnt=0
-for i in permutations(arr,N):
+for i in permutations(A, N):
     w=500
     for j in i:
         w+=j-K
@@ -14,25 +14,44 @@ for i in permutations(arr,N):
     if w>=500: cnt+=1            
 print(cnt)
 
-풀이2) 백트래킹
+# 풀이2) 백트래킹1
 import sys
 input=sys.stdin.readline
 N, K = map(int, input().rstrip().split())
 A = list(map(int, input().rstrip().split()))
-ans = 0
+cnt = 0
 visited = [False] * N
 def dfs(n, weight):
-    global ans
-    if weight < 500:
-        return
+    global cnt
     if n == N:
-        ans += 1
+        cnt += 1
         return
-    weight -= K
     for i in range(N):
-        if not visited[i]:
+        if not visited[i] and weight+A[i]-K >= 500 :
             visited[i] = True
-            dfs(n + 1, weight + A[i])
+            dfs(n + 1, weight + A[i]-K)
             visited[i] = False
 dfs(0, 500)
-print(ans)
+print(cnt)
+
+# 풀이3) 백트래킹2
+import sys
+input=sys.stdin.readline
+N, K = map(int, input().rstrip().split())
+A = list(map(int, input().rstrip().split()))
+cnt = 0
+w=500
+visited = [False] * N
+def dfs(n):
+    global cnt,w
+    if n == N:
+        cnt += 1
+        return
+    for i in range(N):
+        if not visited[i] and w+A[i]-K >= 500 :
+            w+=A[i]-K
+            visited[i] = True
+            dfs(n + 1)
+            visited[i] = False
+dfs(0)
+print(cnt)

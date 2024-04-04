dfa443bb0ff71f3aa77c435d80fb990f0c2da8de
Hyeji Choi <hyejichoi14@gmail.com>
2024-04-05 01:18:42 +0900

    Update 22. 백트래킹-02. N과 M (1)[백준 15649].py

--- 22. 백트래킹-02. N과 M (1)[백준 15649].py
+++ 22. 백트래킹-02. N과 M (1)[백준 15649].py
@@ -1,11 +1,14 @@
-# 풀이1) 순열 permutations 이용
-from itertools import permutations # 순열
-N, M = map(int, input().split())
-arr = list(range(1, N+1))
-for i in permutations(arr, M):
-  print(*i,end='\n')
-
-# 풀이2) 백트래킹
+# 풀이1) 백트래킹
+'''
+1.아이디어
+- 백트래킹 재귀함수 안에서, for문 돌면서 숫자 선택(이때 방문여부 체크)
+- 재귀함수에서 M개를 선택할 경우 print 후 return
+2. 시간복잡도
+- (1 ≤ M ≤ N ≤ 8)이고, 중복불가니까 O(N!)로 백트래킹 가능
+3. 자료구조
+- 결과값 저장 int[]
+- 방문여부체크 bool[]
+'''
 import sys
 input=sys.stdin.readline
 N, M=map(int,input().split())
@@ -23,3 +26,12 @@ def backtraking(num):
             visited[i]=False
             arr.pop()
 backtraking(0)
+
+# 풀이2) 순열 permutations 이용
+from itertools import permutations # 순열
+N, M = map(int, input().split())
+arr = list(range(1, N+1))
+for i in permutations(arr, M):
+  print(*i,end='\n')
+
+''' 참고링크: https://youtu.be/atTzqxbt4DM?si=Qgufvn-_GyU6oA52 '''
